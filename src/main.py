068e0e80ac6d8b66c55f7678543dd26f2d0b90c9
minose <minose@live.ca>
2025-04-01 11:05:51 -0700

    typo

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from textnode import TextNode, TextType
 def main():
     # Create a TextNode with dummy values
     # For example, creating a link node
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    node = TextNode("This is some anchor text", TextType.LINKS, "https://www.boot.dev")
     
     # Print the node to see its string representation
     print(node)
