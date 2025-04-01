8ada55a88dd19f422e54c6213b842da4bb727c73
minose <minose@live.ca>
2025-04-01 11:05:22 -0700

    a#

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from textnode import TextNode, TextType
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.bood.dev")
-
+    # Create a TextNode with dummy values
+    # For example, creating a link node
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    
+    # Print the node to see its string representation
     print(node)
 
-
-#call main function
-if __name__ == "__main___":
+# Call the main function
+if __name__ == "__main__":
     main()
