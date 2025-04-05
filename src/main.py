201391c4763d3038b951940e5ac0ae3921bc5e0f
minose <minose@live.ca>
2025-04-04 22:31:15 -0700

    delimiter function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_nodes_delimiter
 
 def main():
     # Create a TextNode with dummy values
     # For example, creating a link node
-    node = TextNode("This is some anchor text", TextType.LINKS, "https://www.boot.dev")
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     
     # Print the node to see its string representation
     print(node)
 
+def text_delimiter():
+    node = TextNode("This is a `delimiter` test", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    for new_node in new_nodes:
+        print(f"Node Contet: {new_node.text}, node type: {new_node.text_type}")
+
 # Call the main function
 if __name__ == "__main__":
     main()
+    text_delimiter()
--- src/textnode.py
+++ src/textnode.py
@@ -51,3 +51,29 @@ def text_node_to_html_node(text_node):
 
     else:
         raise Exception ("Type not supported")
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    
+    for node in old_nodes:
+
+        if node.text_type == TextType.TEXT:
+            if node.text.count(delimiter) % 2 != 0:
+                raise ValueError(f"Unmatched delimiter '{delimiter}' in text: {node.text}")
+            
+            split_lists = node.text.split(delimiter)
+            index = 0
+            for part in split_lists:
+                if index % 2 == 0: #even index plain text
+                    new_nodes.append(TextNode(part, TextType.TEXT))
+                else: #odd delimiter text
+                    new_nodes.append(TextNode(part, text_type))
+                index += 1
+        else:
+            new_nodes.append(node)
+
+    return new_nodes
+               
+            
+            
+
